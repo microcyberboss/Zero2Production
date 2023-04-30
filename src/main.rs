99143c37e0ab3b6ba1e2e331455e396e01e9f609
Dinesh <dineshphuyel20@gmail.com>
2023-04-30 15:27:50 +0545

    write heal_check test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use std::net::TcpListener;
+
+use zero2prod::run;
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+async fn main() -> std::io::Result<()>   {
+   let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+   run(listener)?.await
 }
