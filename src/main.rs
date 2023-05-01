e4acff3b1756c0fad071ca30143a7ef2de7ecb7d
Dinesh <dineshphuyel20@gmail.com>
2023-05-01 22:15:41 +0545

    created databse and run all test sucessfully

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use std::net::TcpListener;
+use sqlx::{PgConnection, Connection, PgPool};
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
-use zero2prod::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()>   {
-   let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-   run(listener)?.await
+   // Panic if we can't read configuration
+   let configuration = get_configuration().expect("Failed to read configuration.");
+
+   let connection_pool = PgPool::connect(&configuration.database.connection_string())
+                  .await
+                  .expect("Failed to connect with protgress"); 
+   // We have removed the hard-coded `8000` - it's now coming from our settings! 
+   let address = format!("127.0.0.1:{}", configuration.application_port);
+   let listener = TcpListener::bind(address)?;
+   run(listener, connection_pool)?.await
 }
